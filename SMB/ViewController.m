7678acccc10cd75fa676eb054ed4d0e1248e7d16
sunkang <958607177@qq.com>
2018-11-03 16:48:58 +0800

    1

--- SMB/ViewController.m
+++ SMB/ViewController.m
@@ -7,16 +7,47 @@
 //
 
 #import "ViewController.h"
+#import "TOSMBClient.h"
+
 
 @interface ViewController ()
+@property (nonatomic, strong, null_resettable) TOSMBSession *session;
 
 @end
 
 @implementation ViewController
 
+- (TOSMBSession *)session {
+    if (!_session) {
+        _session = [[TOSMBSession alloc] initWithIPAddress:@"10.0.1.81"];
+
+        [_session setLoginCredentialsWithUserName:@"username" password:@"password"];
+    }
+    return _session;
+}
+
+
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    NSString *path = [NSString stringWithFormat:@"/sunkang/Desktop/name.text"];//服务器路径
+    
+    NSString *txtPath = [[NSBundle mainBundle] pathForResource:@"sun_kang" ofType:@"rtf"];
+    NSData *data = [[NSData alloc]initWithContentsOfFile:txtPath];//需要上传的文件转data
+    
+    TOSMBSessionUploadTask *uploadTask = [self.session uploadTaskForFileAtPath:path data:data progressHandler:nil completionHandler:^{
+        //ok
+        
+    } failHandler:^(NSError *error) {
+        //no
+    }];
+    
+    [uploadTask resume];
+
+}
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
 }
 
 
